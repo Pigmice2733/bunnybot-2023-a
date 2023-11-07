f431ce27dea48fde429ffb2bfe1d5d07ac83fbd7
LucaHaverty <lucahaverty@gmail.com>
2023-11-06 16:31:07 -0800

    The start to a new and improved state machine

--- src/main/java/frc/robot/commands/RunTurretStateMachine.java
+++ src/main/java/frc/robot/commands/RunTurretStateMachine.java
@@ -4,29 +4,62 @@
 
 package frc.robot.commands;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.function.DoubleSupplier;
 
+import org.photonvision.targeting.PhotonTrackedTarget;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.TurretStateMachine;
-import frc.robot.TurretStateMachine.TurretStates;
 import frc.robot.subsystems.Turret;
 import frc.robot.subsystems.Vision;
+import frc.robot.turret_state_machine.FiniteStateMachine;
+import frc.robot.turret_state_machine.Transition;
 
 public class RunTurretStateMachine extends CommandBase {
-    private final TurretStateMachine stateMachine;
+    private final FiniteStateMachine<TurretState, TurretData> stateMachine;
+
+    private final Turret turret;
+    private final Vision vision;
+
+    private final DoubleSupplier manualRotationSpeed;
 
     public RunTurretStateMachine(Turret turret, Vision vision, DoubleSupplier manualRotationSpeed) {
-        this.stateMachine = new TurretStateMachine(turret, vision, manualRotationSpeed);
-        addRequirements(turret, vision);
-    }
+        stateMachine = new FiniteStateMachine<TurretState, TurretData>(TurretState.BeginWander,
+                new HashMap<TurretState, List<Transition<TurretState, TurretData>>>());
 
-    @Override
-    public void initialize() {
-        stateMachine.setState(TurretStates.WANDER_RIGHT);
+        this.turret = turret;
+        this.vision = vision;
+        this.manualRotationSpeed = manualRotationSpeed;
     }
 
     @Override
     public void execute() {
-        stateMachine.updateStateMachine();
+        // PhotonTrackedTarget target = vision.getCurrentTarget();
+
+        // if (target == null) {
+        // setState(TurretStates.TRACK_TARGET);
+        // return;
+        // }
+
+        stateMachine.execute(new TurretData(turret.getTurretVelocity(), manualRotationSpeed.getAsDouble()));
+    }
+
+    public enum TurretState {
+        BeginWander,
+        WanderLeft,
+        WanderRight,
+        TrackTarget,
+        Manual
+    }
+
+    public class TurretData {
+        public final double turretVelocity;
+        public final double manualRotationSpeed;
+
+        public TurretData(double turretVelocity, double manualRotationSpeed) {
+            this.turretVelocity = turretVelocity;
+            this.manualRotationSpeed = manualRotationSpeed;
+        }
     }
 }
