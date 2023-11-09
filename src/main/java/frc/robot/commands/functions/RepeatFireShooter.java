08c6dd14fbb9b434475c22f378c837c9b5856b81
LucaHaverty <lucahaverty@gmail.com>
2023-11-09 15:42:51 -0800

    Repeat fire shooter

---
+++ src/main/java/frc/robot/commands/functions/RepeatFireShooter.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.functions;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.AutoConfig;
+import frc.robot.Constants.ShooterConfig;
+import frc.robot.commands.actions.FeedShooter;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Shooter;
+
+public class RepeatFireShooter extends SequentialCommandGroup {
+    /** Fires the shooter at a set interval until canceled */
+    public RepeatFireShooter(Indexer indexer, Shooter shooter) {
+        addCommands(shooter.setFlywheelSpeed(ShooterConfig.DEFAULT_OUTPUT),
+                Commands.waitSeconds(AutoConfig.SHOOTER_SPINUP_TIME),
+                Commands.repeatingSequence(
+                        new FeedShooter(indexer),
+                        Commands.waitSeconds(AutoConfig.TIME_BETWEEN_SHOTS)));
+    }
+}
